0622848671246e227980bbe53d567d236bd1e69b
MangaTracker <brylelariosa12345@gmail.com>
2026-05-17 23:21:23 +0800

    edit: update SMAPIGameLoader/Game/NativeLibManager.cs via ZipApkBuilder [skip ci]

--- SMAPIGameLoader/Game/NativeLibManager.cs
+++ SMAPIGameLoader/Game/NativeLibManager.cs
@@ -1,11 +1,6 @@
-﻿using LWJGL;
-using MonoGame.Framework.Utilities;
 using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Runtime.InteropServices;
 
 namespace SMAPIGameLoader.Game;
 
@@ -13,21 +8,21 @@ internal static class NativeLibManager
 {
     static nint Load_libLZ4()
     {
-        nint num = FuncLoader.LoadLibrary("liblwjgl_lz4.so");
-        if (num == IntPtr.Zero)
-        {
-            string folderPath = System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal);
-            string directoryName = Path.GetDirectoryName(folderPath);
-            string libname = Path.Combine(directoryName, "lib", "liblwjgl_lz4.so");
-            num = FuncLoader.LoadLibrary(libname);
-        }
-        return num;
+        if (NativeLibrary.TryLoad("liblwjgl_lz4.so", out nint handle))
+            return handle;
+
+        string folderPath = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+        string directoryName = Path.GetDirectoryName(folderPath);
+        string libname = Path.Combine(directoryName, "lib", "liblwjgl_lz4.so");
+        NativeLibrary.TryLoad(libname, out handle);
+        return handle;
     }
+
     public static void Loads()
     {
         try
         {
-            int b = LZ4.CompressBound(10);
+            Load_libLZ4();
             Console.WriteLine("done setup native libs");
         }
         catch (Exception ex)
